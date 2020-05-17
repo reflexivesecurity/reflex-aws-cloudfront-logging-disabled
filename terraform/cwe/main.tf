befc08fd63d1691d1a5eff36d98f9e4c84f8d62e
Richard Julian <richard@rjulian.net>
2020-05-17 12:47:39 -0700

    Patch: Fix CWE module source

--- terraform/cwe/main.tf
+++ terraform/cwe/main.tf
@@ -1,5 +1,5 @@
 module "cwe" {
-  source      = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.6.0"
+  source      = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe?ref=v0.6.0"
   name        = "CloudfrontLoggingDisabled"
   description = "A reflex rule to detect when CloudFront logging is disabled."
 
